fe14f74ce16f4fed0fac6399ab7898a10a28f650
disney <disney@tokeninsight.com>
2023-04-06 13:22:39 +0800

    add loop

--- app/components/HeadVideo.tsx
+++ app/components/HeadVideo.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import styles from "./HeadVideo.module.scss";
+
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 const EX_HUMAN_TOKEN =
   "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6Im1pa2tvLmlrb2xhQGdtYWlsLmNvbSJ9.q8d45E7TJ5gSwg45SKAsztXqxB3Hapz6l1rnnUU73-6RSJoCbqS4HQWL57HxXZM7rOmhAl1ULmkgmQcA0nFX2g";
@@ -7,11 +11,11 @@ const EX_HUMAN_TOKEN =
 const exHumanEndpoint = "https://api.exh.ai/animations/v1/generate_lipsync";
 
 export default function HeadVideo() {
-  const videoRef = useRef<HTMLVideoElement>(null);
   const [question, setQuestion] = useState<string>("How are u?");
   const [videoUrl, setVideoUrl] = useState<string>(
     "https://ugc-idle.s3-us-west-2.amazonaws.com/415417c29c16d10016b9042a44cd3e09.mp4"
   );
+  const [playing, setPlaying] = useState<boolean>(false);
 
   async function getVideo() {
     // videoRef.current?.pause();
@@ -39,27 +43,45 @@ export default function HeadVideo() {
       let blob = await responseStream.blob();
       let url = URL.createObjectURL(blob);
       setVideoUrl(url);
-      setTimeout(() => {
-        videoRef.current?.play();
-      }, 0);
+      setPlaying(true);
     } else {
       // Suggest showing a friendly error to the user. This would "quietly" fail.
       console.log("video url unknown");
     }
   }
 
-  //   useEffect(() => {
-  //     getVideo();
-  //   }, []);
-
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     getVideo();
   };
+
   return (
-    <div>
-      <div className="head-video">
-        <video ref={videoRef} playsInline src={videoUrl as string} />
+    <div className={styles["container"]}>
+      <div className={styles["head-video"]}>
+        {playing && (
+          <ReactPlayer
+            className={`${styles["react-player"]} ${styles["front"]}`}
+            playing={playing}
+            onEnded={() => {
+              setPlaying(false);
+            }}
+            playsinline
+            url={videoUrl}
+            width="100%"
+            height="100%"
+          />
+        )}
+        <ReactPlayer
+          className={styles["react-player"]}
+          playing={true}
+          loop
+          playsinline
+          url={
+            "https://ugc-idle.s3-us-west-2.amazonaws.com/415417c29c16d10016b9042a44cd3e09.mp4"
+          }
+          width="100%"
+          height="100%"
+        />
       </div>
       <div>
         <input
