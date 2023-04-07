5b116b6bd9a4db78277858e5e2581428d4e7a5ff
disney <disney@tokeninsight.com>
2023-04-07 21:04:07 +0800

    change title

--- app/components/Mentors.tsx
+++ app/components/Mentors.tsx
@@ -9,7 +9,7 @@ export default function Mentors({
 }) {
   return (
     <div className="artboard phone-1">
-      <h1 className="text-center font-bold text-lg">Mentor Machines</h1>
+      <h1 className="text-center font-bold text-4xl">Mentor Machines</h1>
       {mentors.map((mentor, index) => {
         return (
           <Fragment key={mentor.name}>
